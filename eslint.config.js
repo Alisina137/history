0edc993bc324e53733a45bc50f6906d2a87da4f3
Alisina <alisinasarwari137@gmail.com>
2026-08-09 15:08:00 +0430

    task 2.1 part 4: fix - install @eslint/js and add type module to package.json

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+      'no-console': 'off',
+      'prefer-const': 'error',
+      'no-var': 'error',
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        console: 'readonly',
+        localStorage: 'readonly',
+        fetch: 'readonly',
+      },
+    },
+  },
+];
