b73b6fc5b301dbd0af0ccf836a08d517366a640f
Alisina <alisinasarwari137@gmail.com>
2026-08-10 07:42:42 +0430

    task 9.2: complete part 1 - add explicit width and height to images

--- build.js
+++ build.js
@@ -338,7 +338,7 @@ function renderEventCard(event, isHero = false) {
   }
 
   const imageBlock = imageUrl
-    ? `<div class="event-card__image"><img src="${imageUrl}" alt="" loading="lazy"></div>`
+    ? `<div class="event-card__image"><img src="${imageUrl}" alt="" loading="lazy" width="400" height="225"></div>`
     : '';
 
   return `
